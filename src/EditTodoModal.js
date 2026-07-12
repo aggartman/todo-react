a18e06d4404b4ed99d027057cf8a4a0bc639fc7f
Austin Rizer <aggrizer@gmail.com>
2026-07-12 16:07:33 -0500

    Add disabled attribute to modal

--- src/EditTodoModal.js
+++ src/EditTodoModal.js
@@ -24,7 +24,11 @@ export default function EditTodoModal({ todo, onSave, onCancel }) {
           onChange={(e) => setName(e.target.value)}
           onKeyDown={handleKeyDown}
         />
-        <button className="modal-confirm-button" onClick={handleSave}>
+        <button
+          className="modal-confirm-button"
+          onClick={handleSave}
+          disabled={name.trim() === ''}
+        >
           Confirm
         </button>
         <button className="modal-cancel-button" onClick={onCancel}>
